11a216284a00652508ca38022674c65569c638df
Joshua Eze <jezeadiebuo5@gmail.com>
2024-10-11 13:18:02 +0100

    added new files to gitignore

--- src/db/docker-entrypoint-initdb.d/importscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Import 'users' collection
-mongoimport --db='oyinyecouture' --collection='users' --file='/tmp/users.json' --jsonArray --username='oyinye' --password='9pZToRnwda6wY97p' --authenticationDatabase=admin
-
-# Import 'products' collection
-mongoimport --db='oyinyecouture' --collection='products' --file='/tmp/products.json' --jsonArray --username='oyinye' --password='9pZToRnwda6wY97p' --authenticationDatabase=admin
